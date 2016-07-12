e655787222e9639fda2e8d64ca4d48803d0f4284
Cagatay Gurturk <cagatay@itusozluk.com>
2016-07-12 14:52:45 +0200

    Lambada version upgraded to 0.0.6

--- src/main/java/org/lambadaframework/example/controllers/ExampleController.java
+++ src/main/java/org/lambadaframework/example/controllers/ExampleController.java
@@ -2,7 +2,12 @@ package org.lambadaframework.example.controllers;
 
 import org.apache.log4j.Logger;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/")
@@ -52,4 +57,27 @@ public class ExampleController {
                 .entity(new Entity(name))
                 .build();
     }
+
+    public static class NewEntityRequest {
+        public String name;
+    }
+
+    /**
+     * This controller uses automatically serialization of Request body to any POJO
+     * @param requestEntity Request Entity
+     * @return Response
+     */
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/resource")
+    public Response exampleSecondEndpointPost(
+            NewEntityRequest requestEntity
+    ) {
+
+        logger.debug("Request got");
+        return Response.status(201)
+                .entity(new Entity(requestEntity.name))
+                .build();
+    }
+
 }
