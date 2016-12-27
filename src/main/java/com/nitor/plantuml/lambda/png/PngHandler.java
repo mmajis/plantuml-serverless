c1ec35eea897f7d8c2ce2ed3978d1fbb76e62520
Mika Majakorpi <mika@majakorpi.net>
2016-12-27 14:03:21 +0200

    Add graphviz dot support

--- src/main/java/com/nitor/plantuml/lambda/png/PngHandler.java
+++ src/main/java/com/nitor/plantuml/lambda/png/PngHandler.java
@@ -13,6 +13,8 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.attribute.PosixFilePermissions;
 import java.util.Base64;
 import java.util.Optional;
 
@@ -20,14 +22,34 @@ public class PngHandler implements RequestStreamHandler {
 
   private static final String ENV_VAR_KEY_STAGE = "stage";
   private static final String DEFAULT_STAGE = "dev";
+  private static final String GRAPHVIZ_DOT = "GRAPHVIZ_DOT";
+  private static final String LAMBDA_TASK_ROOT = "LAMBDA_TASK_ROOT";
+  private static final String DOT_PATH = "/tmp/dot_static";
 
   private static final Logger logger = Logger.getLogger(PngHandler.class);
+
   private JSONParser parser = new JSONParser();
+
 
   static {
     String stage = Optional.ofNullable(System.getenv(ENV_VAR_KEY_STAGE)).orElse(DEFAULT_STAGE);
     LogManager.resetConfiguration();
     PropertyConfigurator.configure(String.format("log4j-%s.properties", stage));
+
+    if (System.getenv(LAMBDA_TASK_ROOT) == null) {
+      logger.error(String.format("%s environment variable is not set. Rendering without graphviz dot!", LAMBDA_TASK_ROOT));
+    } else {
+      String taskRootDotPath = String.format("%s/dot_static", System.getenv(LAMBDA_TASK_ROOT));
+      try {
+        File dotFile = new File(DOT_PATH);
+        Files.copy(new File(taskRootDotPath).toPath(), dotFile.toPath());
+        Files.setPosixFilePermissions(dotFile.toPath(), PosixFilePermissions.fromString("rwxr-xr-x"));
+        System.setProperty(GRAPHVIZ_DOT, DOT_PATH);
+      } catch (IOException e) {
+        logger.error(String.format("Failed to copy graphviz dot executable to %s. Rendering without graphviz dot!", DOT_PATH), e);
+      }
+    }
+    logger.debug(String.format("GRAPHVIZ_DOT system property: %s", System.getProperty(GRAPHVIZ_DOT)));
   }
 
   @Override
@@ -50,7 +72,7 @@ public class PngHandler implements RequestStreamHandler {
       }
       JSONObject headerJson = new JSONObject();
       headerJson.put("Access-Control-Allow-Origin", "*");
-      headerJson.put("Content-Type", "image/png"); //This is dirty but works as long as browser does content type sniffing...
+      headerJson.put("Content-Type", "image/png");
 
       ByteArrayOutputStream baos = handleUml(encodedUml);
       byte[] bytes = baos.toByteArray();
