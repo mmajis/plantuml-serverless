749bf82a948029849a63269c6e764e3763fedf59
Mika Majakorpi <mika.majakorpi@nitor.com>
2018-04-29 12:41:04 +0300

    Use relative paths to templates

--- sam-deploy.sh
+++ sam-deploy.sh
@@ -3,9 +3,9 @@
 aws cloudformation package \
 --s3-bucket plantuml-serverlessrepo \
 --output-template-file target/serverless-output.yaml \
---template-file /Users/mika/code/plantuml-serverless/sam-template.yml
+--template-file sam-template.yml
 
 aws cloudformation deploy \
---template-file /Users/mika/code/plantuml-serverless/target/serverless-output.yaml \
+--template-file target/serverless-output.yaml \
 --stack-name sam-plantuml \
 --capabilities CAPABILITY_IAM
