9df434723c501b41585587f404b5d7b7fef98c1e
Mika Majakorpi <mika.majakorpi@nitor.com>
2021-06-14 19:46:08 +0300

    v0.0.9

--- serverlessrepo/serverlessrepo-publish.sh
+++ serverlessrepo/serverlessrepo-publish.sh
@@ -1,5 +1,5 @@
-aws --region us-east-1 serverlessrepo create-application-version --application-id arn:aws:serverlessrepo:us-east-1:293246570391:applications/plantuml-render --semantic-version 0.0.8 --source-code-url https://github.com/mmajis/plantuml-serverless/releases/tag/v0.0.8-fixed --template-body file://.aws-sam/build/template.yaml
+aws --region us-east-1 serverlessrepo create-application-version --application-id arn:aws:serverlessrepo:us-east-1:293246570391:applications/plantuml-render --semantic-version 0.0.8 --source-code-url https://github.com/mmajis/plantuml-serverless/releases/tag/v0.0.9 --template-body file://.aws-sam/build/template.yaml
 #maybe this could be the future:
-#sam publish --region us-east-1 --template .aws-sam/build/template.yaml --semantic-version 0.0.8
+#sam publish --region us-east-1 --template .aws-sam/build/template.yaml --semantic-version 0.0.9
 
 # The issue here is that serverless application repository doesn't appear to support lambdas with PackageType: Image (containers)
