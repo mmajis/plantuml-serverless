91808a08c14b5fdbf5a2c630684cb211cba49f2d
Mika Majakorpi <mika.majakorpi@nitor.com>
2021-06-15 15:16:47 +0300

    Use profile param in default values queried with AWS CLI

--- sam-deploy.sh
+++ sam-deploy.sh
@@ -11,25 +11,29 @@ $(basename "${BASH_SOURCE[0]}") -b <s3bucket> [ -s <stackName> ] [ -p <awsProfil
 Options
   -b | --bucket       (Required) The S3 bucket to upload artifacts to.
   -s | --stack-name   The name of the CloudFormation stack to create.
-                      (default: sam-plantuml)
+                      (default: plantuml-sam)
   -p | --profile      Specify an AWS profile
   -r | --region       Specify an AWS region
+  -a | --account      Specify container image repository account id
+  -i | --image-repo)  Specify container image repository name
+                      (default: plantuml-sam)
   -h | --help         Print this usage message and exit
 EOUSAGE
 }
 awsProfile=()
-awsRegion="$(aws ec2 describe-availability-zones --output text --query 'AvailabilityZones[0].[RegionName]')"
+rawRegion=""
+awsRegion=()
 s3bucket=""
 stackName="plantuml-sam"
 repoName="plantuml-sam"
-accountId="$(aws sts get-caller-identity --query Account --output text)"
+accountId=""
 while [[ $# -gt 0 ]]; do
   opt="$1"
   shift
   case "$opt" in
     -h|--help)        usage; exit;;
     -p|--profile)     awsProfile=(--profile "$1"); shift;;
-    -r|--region)      awsRegion=(--region "$1"); shift;;
+    -r|--region)      rawRegion="$1"; awsRegion=(--region "$1"); shift;;
     -b|--bucket)      s3bucket="$1"; shift;;
     -s|--stack-name)  stackName="$1"; shift;;
     -a|--account)     accountId="$1"; shift;;
@@ -37,6 +41,15 @@ while [[ $# -gt 0 ]]; do
     *)                echo "Unknown option $opt"; usage; exit 1;;
   esac
 done
+
+if [[ "$rawRegion" == "" ]]; then
+  rawRegion="$(aws ${awsProfile[*]} ec2 describe-availability-zones --output text --query 'AvailabilityZones[0].[RegionName]')"
+  awsRegion=(--region "$rawRegion")
+fi
+
+if [[ "$accountId" == "" ]]; then
+  accountId="$(aws ${awsProfile[*]} sts get-caller-identity --query Account --output text)"
+fi
 
 if [[ "$s3bucket" == "" ]]; then
   echo "please provide an S3 bucket name with -b" >&2
@@ -59,15 +72,15 @@ sam build --cached
 
 # Edit the AWS account id and region in the repo URL here
 aws ecr get-login-password | docker login --username AWS \
---password-stdin "${accountId}.dkr.ecr.${awsRegion[*]}.amazonaws.com"
+--password-stdin "${accountId}.dkr.ecr.${rawRegion}.amazonaws.com"
 
 # Replace image-repository with your own
 sam deploy "${awsProfile[@]}" "${awsRegion[@]}" \
 --stack-name "${stackName}" \
 --capabilities CAPABILITY_IAM \
---image-repository "${accountId}.dkr.ecr.${awsRegion[*]}.amazonaws.com/${repoName}" \
+--image-repository "${accountId}.dkr.ecr.${rawRegion}.amazonaws.com/${repoName}" \
 --s3-bucket "${s3bucket}"
 
 #These would be for serverless application repository, but it doesn't support container based lambdas at the moment.
-#aws s3 cp LICENSE s3://"${s3bucket}"/LICENSE
-#aws s3 cp serverlessrepo/README.md s3://"${s3bucket}"/README.md
+#aws ${awsProfile[@]} s3 cp LICENSE s3://"${s3bucket}"/LICENSE
+#aws ${awsProfile[@]} s3 cp serverlessrepo/README.md s3://"${s3bucket}"/README.md
